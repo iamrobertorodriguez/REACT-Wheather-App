619cc8e71849710e674f87942d0f8194ba48d5f3
Roberto Rodriguez <rdrgzmndz.sm0101@gmail.com>
2022-06-17 17:51:02 -0500

    contact updated

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -11,7 +11,7 @@ const LoadingScreen = () => {
     return (
         <div id='loading-page' >
             <img className='loading-icon' src="https://media3.giphy.com/media/OQuEO3CScOAuFYJlBd/200w.gif?cid=82a1493b6xzjclmtzvyyii9q7b5lc7bi7pgq0n60pylfd1q7&rid=200w.gif&ct=s" alt="water drop filling" />
-            <h2>Please allow geolocation for a corret operation...</h2>
+            <h2>Please allow geolocation for a corret operation...<br />If you are using Firefox please enable backdrop-filter for styling purposes.<br /><br />By: iamrobertorodriguez.netlify.app</h2>
         </div>
     );
 };
