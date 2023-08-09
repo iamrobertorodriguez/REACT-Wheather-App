0e14f963756e0e3e17019515f522ecd5e2cbea74
Roberto Rodríguez <98791278+RodriguezRoberto@users.noreply.github.com>
2023-08-08 20:28:00 -0600

    Update LoadingScreen.js

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -5,7 +5,7 @@ const LoadingScreen = () => {
     return (
         <div id='loading-page' >
             <img className='loading-icon' src="https://media3.giphy.com/media/OQuEO3CScOAuFYJlBd/200w.gif?cid=82a1493b6xzjclmtzvyyii9q7b5lc7bi7pgq0n60pylfd1q7&rid=200w.gif&ct=s" alt="water drop filling" />
-            <h2>Before start please allow access to your location for a corret operation...<br />If you are using Firefox please enable backdrop-filter for styling purposes.<br /><br />By: iamrobertorodriguez.netlify.app</h2>
+            <h2>Before start please allow access to your location for a corret operation...<br />If you are using Firefox please enable backdrop-filter for styling purposes.<br /><br />By: www.iamrobertorodriguez.com</h2>
         </div>
     );
 };
